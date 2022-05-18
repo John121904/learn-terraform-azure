92ac13abae81a97295ed1c5bc831a2bfd5460d9e
John Marcial <johnmarloumarcial@gmail.com>
2022-05-18 13:03:58 +0800

    added backend block to main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,12 @@ terraform {
       version = "~> 2.65"
     }
   }
-
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate8765"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_version = ">= 1.1.0"
 }
 
